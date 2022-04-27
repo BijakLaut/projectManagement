50ed705667aa52885930df2fe5e0a503f375dd96
BijakLaut <hudaaputra72@gmail.com>
2022-04-27 20:45:42 +0700

    Penambahan Method Edit Job

--- app/Controllers/Job.php
+++ app/Controllers/Job.php
@@ -30,4 +30,35 @@ class Job extends BaseController
 
         return view('job/jobDetail', $data);
     }
+
+    public function editJob()
+    {
+        $this->jobModel->save([
+            'job_id' => $this->request->getVar('job_id'),
+            'name' => $this->request->getVar('name'),
+            'progress' => $this->request->getVar('progress'),
+            'duedate' => $this->request->getVar('duedate'),
+            'unit_id' => $this->request->getVar('unit_id')
+        ]);
+
+        $unitProgress = $this->jobModel->getJobProgress($this->request->getVar('unit_id'));
+
+        $this->unitModel->save([
+            'unit_id' => $this->request->getVar('unit_id'),
+            'progress' => $unitProgress
+        ]);
+
+        $segmentProgress = $this->unitModel->getUnitProgress($this->request->getVar('segment_id'));
+
+        $this->segmentModel->save([
+            'segment_id' => $this->request->getVar('segment_id'),
+            'progress' => $segmentProgress
+        ]);
+        // dd($unitProgress);
+        // print_r(array_reduce($array, "own_function", "2"));
+
+        session()->setFlashdata('pesan', 'Pekerjaan berhasil diubah');
+
+        return redirect()->back();
+    }
 }
